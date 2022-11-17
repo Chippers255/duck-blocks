492b1765a3fd9c667fabb49d2a781e3f687fc8d7
Chippers255 <tn90ca@gmail.com>
2022-11-16 23:47:46 -0500

    :sparkles: started build basic blockchain functions

---
+++ blockchain/blockchain.go
@@ -0,0 +1,89 @@
+package blockchain
+
+import (
+	"time"
+
+	"github.com/duck-dynasty/duck-blocks/accounts"
+	"github.com/duck-dynasty/duck-blocks/transactions"
+)
+
+type Block struct {
+	Index         int                              `json:"index"`
+	Proof         string                           `json:"proof"`
+	Timestamp     int64                            `json:"timestamp"`
+	Transactions  []transactions.DuckatTransaction `json:"transactions"`
+	PreviousHash  string                           `json:"previous_hash"`
+	AccountStates map[string]accounts.AccountState `json:"account_states"`
+}
+
+type Blockchain struct {
+	Chain               []Block                          `json:"chain"`
+	CurrentTransactions []transactions.DuckatTransaction `json:"current_transactions"`
+	CurrentStates       map[string]accounts.AccountState `json:"account_states"`
+}
+
+func (b *Blockchain) LastBlock() Block {
+	return b.Chain[len(b.Chain)-1]
+}
+
+func (b *Blockchain) UpdateFromState(address string, value int64) {
+	state, _ := b.CurrentStates[address]
+	state.Nonce += 1
+	state.Value -= value
+	b.CurrentStates[address] = state
+}
+
+func (b *Blockchain) UpdateToState(address string, value int64) {
+	state, ok := b.CurrentStates[address]
+	if ok {
+		state.Value += value
+	} else {
+		state = accounts.AccountState{Address: address, Value: value, Code: nil, Data: nil, Nonce: 0}
+	}
+
+	b.CurrentStates[address] = state
+}
+
+func (b *Blockchain) NewTransaction(newTransaction transactions.DuckatTransaction) bool {
+	accountState, ok := b.CurrentStates[newTransaction.Params.From]
+
+	if newTransaction.VerifyTransaction() {
+		if ok {
+			if accountState.Value >= newTransaction.Params.Amount {
+				b.CurrentTransactions = append(b.CurrentTransactions, newTransaction)
+				b.UpdateFromState(newTransaction.Params.From, newTransaction.Params.Amount)
+				b.UpdateToState(newTransaction.Params.To, newTransaction.Params.Amount)
+			}
+		}
+	}
+
+	return false
+}
+
+func (b *Blockchain) NewBlock(proof string, previousHash string) Block {
+	block := Block{
+		Index:         len(b.Chain) + 1,
+		Proof:         proof,
+		Timestamp:     time.Now().Unix(),
+		Transactions:  b.CurrentTransactions,
+		PreviousHash:  previousHash,
+		AccountStates: make(map[string]accounts.AccountState),
+	}
+
+	b.CurrentTransactions = nil
+	b.Chain = append(b.Chain, block)
+
+	return block
+}
+
+func NewBlockchain() Blockchain {
+	blockchain := Blockchain{
+		Chain:               []Block{},
+		CurrentTransactions: []transactions.DuckatTransaction{},
+		CurrentStates:       make(map[string]accounts.AccountState),
+	}
+
+	_ = blockchain.NewBlock("100", "1")
+
+	return blockchain
+}
--- main.go
+++ main.go
@@ -1,36 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
-	accounts "github.com/duck-dynasty/duck-blocks/accounts"
-	core "github.com/duck-dynasty/duck-blocks/core"
+	"github.com/duck-dynasty/duck-blocks/accounts"
+	"github.com/duck-dynasty/duck-blocks/blockchain"
 )
 
 func main() {
 	fmt.Println("--------------- USER ACCOUNT TESTS ---------------")
 	u1 := accounts.NewUserAccount()
 	fmt.Println(u1.JSON())
-	u1.SaveKeys()
+
+	u2 := accounts.NewUserAccount()
+	fmt.Println(u2.JSON())
+
+	u3 := accounts.NewUserAccount()
+	fmt.Println(u3.JSON())
+
+	u4 := accounts.NewUserAccount()
+	fmt.Println(u4.JSON())
 	fmt.Println("--------------- USER ACCOUNT TESTS ---------------")
 	fmt.Println("")
 
 	fmt.Println("------------- USER TRANSACTION TESTS -------------")
-	t := u1.CreateTransaction("tom", 5000)
-	fmt.Println(t.GetJSON())
-	t.VerifyTransaction()
+	t1 := u1.CreateTransaction(u2.Address, 50)
+	fmt.Println(t1.GetJSON())
+	fmt.Println(t1.VerifyTransaction())
+
+	t2 := u2.CreateTransaction(u4.Address, 20)
+	fmt.Println(t2.GetJSON())
+	fmt.Println(t2.VerifyTransaction())
+
+	t3 := u3.CreateTransaction(u1.Address, 10)
+	fmt.Println(t3.GetJSON())
+	fmt.Println(t3.VerifyTransaction())
+
+	t4 := u3.CreateTransaction(u4.Address, 15)
+	fmt.Println(t4.GetJSON())
+	fmt.Println(t4.VerifyTransaction())
 	fmt.Println("------------- USER TRANSACTION TESTS -------------")
 	fmt.Println("")
 
-	fmt.Println("---------------- HEX ENCODE TESTS ----------------")
-	fmt.Println("OG PUBLIC KEY: ", u1.PublicKey)
-	hs := core.PublicKeyToHex(u1.PublicKey)
-	fmt.Println("ENCODED PUBLIC KEY: ", hs)
-	pk := core.HexToPublicKey(hs)
-	fmt.Println("DECODED PUBLIC KEY: ", pk)
-	fmt.Println("---------------- HEX ENCODE TESTS ----------------")
-	fmt.Println("")
+	s1 := accounts.AccountState{Address: u1.Address, Value: 100, Code: nil, Data: nil, Nonce: 0}
+	s2 := accounts.AccountState{Address: u2.Address, Value: 100, Code: nil, Data: nil, Nonce: 0}
+	s3 := accounts.AccountState{Address: u3.Address, Value: 100, Code: nil, Data: nil, Nonce: 0}
 
+	bc := blockchain.NewBlockchain()
+	bc.Chain[0].AccountStates[u1.Address] = s1
+	bc.Chain[0].AccountStates[u2.Address] = s2
+	bc.Chain[0].AccountStates[u3.Address] = s3
+	bc.CurrentStates[u1.Address] = s1
+	bc.CurrentStates[u2.Address] = s2
+	bc.CurrentStates[u3.Address] = s3
+	bc.NewTransaction(t1)
+	bc.NewTransaction(t2)
+	bc.NewTransaction(t3)
+	bc.NewTransaction(t4)
+
+	value, _ := json.MarshalIndent(bc, "", "    ")
+	fmt.Println(string(value))
 	/*
 		tv := transactions.DuckartParams{Amount: 1000000000000000000, From: "Tom", MaxFee: 12345, Nonce: 1, To: "Tim", Type: 127}
 		t1 := transactions.DuckatTransaction{Params: &tv}
